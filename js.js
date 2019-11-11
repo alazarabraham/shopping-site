5a76b6812eb8c01e1d69cb5514bff5ba8c4c03d7
Alazar Markos <alazarabraham1@gmail.com>
2019-11-11 00:23:47 -0500

    fourth commit

--- js.js
+++ js.js
@@ -1,24 +1,42 @@
 // QUANTITY
 function buyNow(){
     const input = document.getElementById('input');
-    console.log(input);
     const amount = input.value;
     console.log(amount);
     const quantity = parseInt(amount);
-    if(isNaN (quantity > 0)){
+    if(isNaN (quantity)){
         const customerInput = document.getElementById("wrongInput");
+        customerInput = "";
         customerInput.innerHTML = "Numbers only"
     }
-    else{
+    else if(quantity>0){
         const customerInput = document.getElementById("wrongInput");
-        customerInput.innerHTML = ("Checking on those " + quantity + " orders!")
+        customerInput.innerHTML = ("Checking on those " + quantity + " orders!"); 
     }
   
-    }
+  
+    
+
+    const grabPrice = document.getElementsByClassName('priceOne').item(0)
+    let price = grabPrice.innerText
+    priceParced = parseInt(price);
+    console.log(priceParced)
+    console.log(price)
+    const cartTotal = amount *  priceParced;
+    console.log(cartTotal);
+    const priceOutput = document.getElementById("priceOutput");
+
+
+
+    priceOutput.innerHTML = ("$"+ cartTotal+ " is your current total");
+    
+
+    
+
+}
 
 //REMOVE ITEMS
 const removeButtons = document.getElementsByClassName('test')
-console.log(removeButtons)
 for(let i = 0; i< removeButtons.length; i ++){
     const button = removeButtons[i].addEventListener('click', ()=>{
         const buttonClicked = event.target
@@ -26,5 +44,7 @@ for(let i = 0; i< removeButtons.length; i ++){
         const customerInput = document.getElementById("wrongInput");
 
         customerInput.innerHTML = "Item removed!"
+        updateCart();
     })
 }
+
